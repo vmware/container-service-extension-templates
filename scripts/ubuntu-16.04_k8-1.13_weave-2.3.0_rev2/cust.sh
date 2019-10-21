621369281d588f3d454a7169f8a2a22a1bce4df3
Andrew Ni <niandrew7@gmail.com>
2019-10-21 15:18:39 -0700

    Update scripts
    Signed-off-by: Andrew Ni <niandrew7@gmail.com>

--- scripts/ubuntu-16.04_k8-1.13_weave-2.3.0_rev2/cust.sh
+++ scripts/ubuntu-16.04_k8-1.13_weave-2.3.0_rev2/cust.sh
@@ -2,20 +2,28 @@
 
 set -e
 
+# disable ipv6 to avoid possible connection errors
 echo 'net.ipv6.conf.all.disable_ipv6 = 1' >> /etc/sysctl.conf
 echo 'net.ipv6.conf.default.disable_ipv6 = 1' >> /etc/sysctl.conf
 echo 'net.ipv6.conf.lo.disable_ipv6 = 1' >> /etc/sysctl.conf
+sudo sysctl -p
+
 echo 'nameserver 8.8.8.8' >> /etc/resolvconf/resolv.conf.d/tail
 resolvconf -u
+
 systemctl restart networking.service
 while [ `systemctl is-active networking` != 'active' ]; do echo 'waiting for network'; sleep 5; done
 
 growpart /dev/sda 1 || :
 resize2fs /dev/sda1 || :
 
+# redundancy: https://github.com/vmware/container-service-extension/issues/432
+systemctl restart networking.service
+while [ `systemctl is-active networking` != 'active' ]; do echo 'waiting for network'; sleep 5; done
+
 echo 'installing kubernetes'
 export DEBIAN_FRONTEND=noninteractive
-apt-get -q update
+apt-get -q update -o Acquire::Retries=3 -o Acquire::http::No-Cache=True -o Acquire::http::Timeout=20 -o Acquire::https::No-Cache=True -o Acquire::https::Timeout=20 -o Acquire::ftp::Timeout=20
 apt-get -q install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
@@ -23,7 +31,7 @@ cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get -q update
+apt-get -q update -o Acquire::Retries=3 -o Acquire::http::No-Cache=True -o Acquire::http::Timeout=20 -o Acquire::https::No-Cache=True -o Acquire::https::Timeout=20 -o Acquire::ftp::Timeout=20
 apt-get -q install -y docker-ce=5:18.09.7~3-0~ubuntu-xenial
 apt-get -q install -y kubelet=1.13.12-00 kubeadm=1.13.12-00 kubectl=1.13.12-00 kubernetes-cni=0.7.5-00 --allow-unauthenticated
 systemctl restart docker
@@ -51,7 +59,7 @@ apt-mark hold nfs-common
 apt-mark hold nfs-kernel-server
 
 echo 'upgrading the system'
-apt-get update
+apt-get -q update -o Acquire::Retries=3 -o Acquire::http::No-Cache=True -o Acquire::http::Timeout=20 -o Acquire::https::No-Cache=True -o Acquire::https::Timeout=20 -o Acquire::ftp::Timeout=20
 apt-get -y -q -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
 
 # /etc/machine-id must be empty so that new machine-id gets assigned on boot (in our case boot is vApp deployment)
