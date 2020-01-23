eb4e5d78b1dc4f3944555eb2a6462728b90c2ece
Andrew Ni <niandrew7@gmail.com>
2020-01-23 13:44:46 -0800

    Update templates
    update photon 1.14 template to use kubernetes 1.14.6-3 (includes fix for CVE-2019-11253)
    update ubuntu 1.15 template to use kubernetes 1.15.9
    update ubuntu 1.16 template to use kubernetes 1.16.6
    
    Signed-off-by: Andrew Ni <niandrew7@gmail.com>

--- scripts/ubuntu-16.04_k8-1.15_weave-2.5.2_rev3/cust.sh
+++ scripts/ubuntu-16.04_k8-1.15_weave-2.5.2_rev3/cust.sh
@@ -33,7 +33,7 @@ EOF
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get -q update -o Acquire::Retries=3 -o Acquire::http::No-Cache=True -o Acquire::http::Timeout=30 -o Acquire::https::No-Cache=True -o Acquire::https::Timeout=30 -o Acquire::ftp::Timeout=30
 apt-get -q install -y docker-ce=5:18.09.7~3-0~ubuntu-xenial
-apt-get -q install -y kubelet=1.15.7-00 kubeadm=1.15.7-00 kubectl=1.15.7-00 kubernetes-cni=0.7.5-00 --allow-unauthenticated
+apt-get -q install -y kubelet=1.15.9-00 kubeadm=1.15.9-00 kubectl=1.15.9-00 kubernetes-cni=0.7.5-00 --allow-unauthenticated
 systemctl restart docker
 while [ `systemctl is-active docker` != 'active' ]; do echo 'waiting for docker'; sleep 5; done
 
--- scripts/ubuntu-16.04_k8-1.16_weave-2.6.0_rev1/cluster-upgrade/worker-k8s-upgrade.sh
+++ scripts/ubuntu-16.04_k8-1.16_weave-2.6.0_rev1/cluster-upgrade/worker-k8s-upgrade.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-echo 'upgrading packages to: kubelet=1.16.4-00 kubeadm=1.16.4-00 kubectl=1.16.4-00 kubernetes-cni=0.7.5-00'
+echo 'upgrading packages to: kubelet=1.16.6-00 kubeadm=1.16.6-00 kubectl=1.16.6-00 kubernetes-cni=0.7.5-00'
 apt-mark unhold kubeadm kubelet kubectl kubernetes-cni
 apt-get -q update -o Acquire::Retries=3 -o Acquire::http::No-Cache=True -o Acquire::http::Timeout=30 -o Acquire::https::No-Cache=True -o Acquire::https::Timeout=30 -o Acquire::ftp::Timeout=30
-apt-get -q install -y kubelet=1.16.4-00 kubeadm=1.16.4-00 kubectl=1.16.4-00 kubernetes-cni=0.7.5-00 --allow-unauthenticated
+apt-get -q install -y kubelet=1.16.6-00 kubeadm=1.16.6-00 kubectl=1.16.6-00 kubernetes-cni=0.7.5-00 --allow-unauthenticated
 apt-mark hold kubeadm kubelet kubectl kubernetes-cni
 
 echo 'upgrading kubeadm in worker node'
