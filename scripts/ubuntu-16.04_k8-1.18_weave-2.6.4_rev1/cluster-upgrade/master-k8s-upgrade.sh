791a829e711d1195bb341c63b5f4d7132d44488b
Aniruddha Shamasundar <aniruddha.9794@gmail.com>
2020-06-09 15:09:43 -0700

    Added k8s 18.3 templates
    Signed-off-by: Aniruddha Shamasundar <aniruddha.9794@gmail.com>

--- scripts/ubuntu-16.04_k8-1.18_weave-2.6.4_rev1/cluster-upgrade/master-k8s-upgrade.sh
+++ scripts/ubuntu-16.04_k8-1.18_weave-2.6.4_rev1/cluster-upgrade/master-k8s-upgrade.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-echo 'upgrading packages to: kubeadm=1.18.2-00'
+echo 'upgrading packages to: kubeadm=1.18.3-00'
 apt-get -q update -o Acquire::Retries=3 -o Acquire::http::No-Cache=True -o Acquire::http::Timeout=30 -o Acquire::https::No-Cache=True -o Acquire::https::Timeout=30 -o Acquire::ftp::Timeout=30
-apt-get install -y --allow-change-held-packages kubeadm=1.18.2-00
+apt-get install -y --allow-change-held-packages kubeadm=1.18.3-00
 
-echo 'upgrading kubeadm to v1.18.2'
+echo 'upgrading kubeadm to v1.18.3'
 while [ `systemctl is-active kubelet` != 'active' ]; do echo 'waiting for kubelet'; sleep 5; done
 # sometimes master will be in 'NotReady' state for a short period
 # api server needs to answer 'OK' to `/healthz`
@@ -14,11 +14,11 @@ while [ `systemctl is-active kubelet` != 'active' ]; do echo 'waiting for kubele
 # upgrade needs to go through various checks
 # master readiness check is not sufficient: while [ `kubectl get nodes | awk '/master/ {print $2}'` != 'Ready,SchedulingDisabled' ]; do echo 'waiting for master to be ready'; sleep 5; done
 sleep 120
-kubeadm upgrade apply v1.18.2 -y
+kubeadm upgrade apply v1.18.3 -y
 
-echo 'upgrading packages to: kubelet=1.18.2-00 kubectl=1.18.2-00 kubernetes-cni=0.7.5-00'
+echo 'upgrading packages to: kubelet=1.18.3-00 kubectl=1.18.3-00 kubernetes-cni=0.8.5-00'
 apt-get -q update -o Acquire::Retries=3 -o Acquire::http::No-Cache=True -o Acquire::http::Timeout=30 -o Acquire::https::No-Cache=True -o Acquire::https::Timeout=30 -o Acquire::ftp::Timeout=30
-apt-get install -y --allow-change-held-packages kubelet=1.18.2-00 kubectl=1.18.2-00 kubernetes-cni=0.7.5-00 --allow-unauthenticated
+apt-get install -y --allow-change-held-packages kubelet=1.18.3-00 kubectl=1.18.3-00 kubernetes-cni=0.8.5-00 --allow-unauthenticated
 
 systemctl restart kubelet
 while [ `systemctl is-active kubelet` != 'active' ]; do echo 'waiting for kubelet'; sleep 5; done
