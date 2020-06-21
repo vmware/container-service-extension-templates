7586cada9e138704205f765648398274f8042d70
Aniruddha Shamasundar <aniruddha.9794@gmail.com>
2020-06-21 13:36:59 -0700

    Address review comments
    Signed-off-by: Aniruddha Shamasundar <aniruddha.9794@gmail.com>

--- scripts/ubuntu-16.04_k8-1.18_weave-2.6.4_rev1/nfsd.sh
+++ scripts/ubuntu-16.04_k8-1.18_weave-2.6.4_rev1/nfsd.sh
@@ -5,3 +5,4 @@ systemctl enable nfs-kernel-server.service
 systemctl start nfs-kernel-server.service
 echo '/home *(rw,sync,no_root_squash,no_subtree_check)' >> /etc/exports
 systemctl restart nfs-kernel-server.service
+
