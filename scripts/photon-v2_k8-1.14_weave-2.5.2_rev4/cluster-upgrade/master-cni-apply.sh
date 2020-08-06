1e1bc33c95cbb5eaaf4e6c248f3c43c44edf8210
Sakthi Sundaram <sakthisunda@yahoo.com>
2020-08-06 14:32:31 -0700

    review comments addressed
    Signed-off-by: Sakthi Sundaram <sakthisunda@yahoo.com>

--- scripts/photon-v2_k8-1.14_weave-2.5.2_rev4/cluster-upgrade/master-cni-apply.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-export kubever=$(kubectl version --client | base64 | tr -d '\n')
-wget --no-verbose -O /root/weave.yml "https://cloud.weave.works/k8s/net?k8s-version=$kubever&v=2.5.2"
-kubectl apply -f /root/weave.yml
-systemctl restart kubelet
-while [ `systemctl is-active kubelet` != 'active' ]; do echo 'waiting for kubelet'; sleep 5; done
