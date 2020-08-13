c9e27370dddb520cd162f8ffaa4e831714b8a7b1
sakthisunda <sakthisunda@yahoo.com>
2020-08-12 17:30:05 -0700

    [VCDA 1575] New K18s 1.18 Template and Template updates (#15)
    * VCDA-1575-Upgrade-Template
    
    Signed-off-by: Sakthi Sundaram <sakthisunda@yahoo.com>
    
    * remove old template version of k8 1.18 as this is the first time this version gets into master
    
    Signed-off-by: Sakthi Sundaram <sakthisunda@yahoo.com>
    
    * Upgrade from k1.17.2 to k1.17.9
    
    Signed-off-by: Sakthi Sundaram <sakthisunda@yahoo.com>
    
    * Upgrade k1.15.9 to k1.15.12
    
    Signed-off-by: Sakthi Sundaram <sakthisunda@yahoo.com>
    
    * upgrade k1.16.6 to k1.16.13
    
    Signed-off-by: Sakthi Sundaram <sakthisunda@yahoo.com>
    
    * photon upgrade
    
    Signed-off-by: Sakthi Sundaram <sakthisunda@yahoo.com>
    
    * review comments addressed
    
    Signed-off-by: Sakthi Sundaram <sakthisunda@yahoo.com>

--- scripts/ubuntu-16.04_k8-1.18_weave-2.6.5_rev1/cluster-upgrade/master-k8s-upgrade.sh
+++ scripts/ubuntu-16.04_k8-1.18_weave-2.6.5_rev1/cluster-upgrade/master-k8s-upgrade.sh
@@ -2,13 +2,11 @@
 
 set -e
 
-echo 'upgrading packages to: kubelet=1.15.12-00 kubeadm=1.15.12-00 kubectl=1.15.12-00 kubernetes-cni=0.7.5-00'
-apt-mark unhold kubeadm kubelet kubectl kubernetes-cni
+echo 'upgrading packages to: kubeadm=1.18.6-00'
 apt-get -q update -o Acquire::Retries=3 -o Acquire::http::No-Cache=True -o Acquire::http::Timeout=30 -o Acquire::https::No-Cache=True -o Acquire::https::Timeout=30 -o Acquire::ftp::Timeout=30
-apt-get -q install -y kubelet=1.15.12-00 kubeadm=1.15.12-00 kubectl=1.15.12-00 kubernetes-cni=0.7.5-00
-apt-mark hold kubeadm kubelet kubectl kubernetes-cni
+apt-get install -y --allow-change-held-packages kubeadm=1.18.6-00 kubernetes-cni=0.8.6-00
 
-echo 'upgrading kubeadm to v1.15.12'
+echo 'upgrading kubeadm to v1.18.6'
 while [ `systemctl is-active kubelet` != 'active' ]; do echo 'waiting for kubelet'; sleep 5; done
 # sometimes master will be in 'NotReady' state for a short period
 # api server needs to answer 'OK' to `/healthz`
@@ -16,7 +14,11 @@ while [ `systemctl is-active kubelet` != 'active' ]; do echo 'waiting for kubele
 # upgrade needs to go through various checks
 # master readiness check is not sufficient: while [ `kubectl get nodes | awk '/master/ {print $2}'` != 'Ready,SchedulingDisabled' ]; do echo 'waiting for master to be ready'; sleep 5; done
 sleep 120
-kubeadm upgrade apply v1.15.12 -y
+kubeadm upgrade apply v1.18.6 -y
+
+echo 'upgrading packages to: kubelet=1.18.6-00 kubectl=1.18.6-00 kubernetes-cni=0.8.6-00'
+apt-get -q update -o Acquire::Retries=3 -o Acquire::http::No-Cache=True -o Acquire::http::Timeout=30 -o Acquire::https::No-Cache=True -o Acquire::https::Timeout=30 -o Acquire::ftp::Timeout=30
+apt-get install -y --allow-change-held-packages kubelet=1.18.6-00 kubectl=1.18.6-00 kubernetes-cni=0.8.6-00 --allow-unauthenticated
 
 systemctl restart kubelet
 while [ `systemctl is-active kubelet` != 'active' ]; do echo 'waiting for kubelet'; sleep 5; done
