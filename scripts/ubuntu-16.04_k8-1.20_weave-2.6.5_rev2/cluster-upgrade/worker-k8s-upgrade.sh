78b05da25f910961596e4d4f04025ce0ad88d28b
Aniruddha Shamasundar <aniruddha.9794@gmail.com>
2021-05-10 16:54:28 -0700

    Script folder changes
    Signed-off-by: Aniruddha Shamasundar <aniruddha.9794@gmail.com>

---
+++ scripts/ubuntu-16.04_k8-1.20_weave-2.6.5_rev2/cluster-upgrade/worker-k8s-upgrade.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo 'upgrading packages to: kubeadm=1.20.5-00'
+apt-get -q update -o Acquire::Retries=3 -o Acquire::http::No-Cache=True -o Acquire::http::Timeout=30 -o Acquire::https::No-Cache=True -o Acquire::https::Timeout=30 -o Acquire::ftp::Timeout=30
+apt-get install -y --allow-change-held-packages kubeadm=1.20.5-00 kubernetes-cni=0.8.7-00
+echo 'upgrading kubeadm in worker node'
+kubeadm upgrade node
+
+echo 'upgrading packages to: kubelet=1.20.5-00 kubectl=1.20.5-00 kubernetes-cni=0.8.7-00'
+apt-get -q update -o Acquire::Retries=3 -o Acquire::http::No-Cache=True -o Acquire::http::Timeout=30 -o Acquire::https::No-Cache=True -o Acquire::https::Timeout=30 -o Acquire::ftp::Timeout=30
+apt-get install -y --allow-change-held-packages kubelet=1.20.5-00 kubectl=1.20.5-00 kubernetes-cni=0.8.7-00
+systemctl restart kubelet
+while [ `systemctl is-active kubelet` != 'active' ]; do echo 'waiting for kubelet'; sleep 5; done
