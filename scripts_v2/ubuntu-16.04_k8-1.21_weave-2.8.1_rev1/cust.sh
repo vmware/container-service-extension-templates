0bcbc6f4cb59b6522ef354f7b77e335c17ac8e35
Andrew Ni <niandrew7@gmail.com>
2021-06-30 11:44:20 -0700

    Add 1.20 and 1.21 to templatesv2; remove 1.16 and 1.17
    Signed-off-by: Andrew Ni <niandrew7@gmail.com>

---
+++ scripts_v2/ubuntu-16.04_k8-1.21_weave-2.8.1_rev1/cust.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+
+set -e
+
+# disable ipv6 to avoid possible connection errors
+echo 'net.ipv6.conf.all.disable_ipv6 = 1' >> /etc/sysctl.conf
+echo 'net.ipv6.conf.default.disable_ipv6 = 1' >> /etc/sysctl.conf
+echo 'net.ipv6.conf.lo.disable_ipv6 = 1' >> /etc/sysctl.conf
+sudo sysctl -p
+
+echo 'nameserver 8.8.8.8' >> /etc/resolvconf/resolv.conf.d/tail
+resolvconf -u
+
+systemctl restart networking.service
+while [ `systemctl is-active networking` != 'active' ]; do echo 'waiting for network'; sleep 5; done
+
+growpart /dev/sda 1 || :
+resize2fs /dev/sda1 || :
+
+# redundancy: https://github.com/vmware/container-service-extension/issues/432
+systemctl restart networking.service
+while [ `systemctl is-active networking` != 'active' ]; do echo 'waiting for network'; sleep 5; done
+
+echo 'installing kubernetes'
+
+docker_ce_version=5:20.10.7~3-0~ubuntu-xenial
+kubernetes_tools_version=1.21.2-00
+kubernetes_cni_version=0.8.7-00
+weave_version=2.8.1
+versioned_weave_file="/root/weave_v$(echo $weave_version | sed -r 's/\./\-/g').yml"
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get -q update -o Acquire::Retries=3 -o Acquire::http::No-Cache=True -o Acquire::http::Timeout=30 -o Acquire::https::No-Cache=True -o Acquire::https::Timeout=30 -o Acquire::ftp::Timeout=30
+apt-get -q install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get -q update -o Acquire::Retries=3 -o Acquire::http::No-Cache=True -o Acquire::http::Timeout=30 -o Acquire::https::No-Cache=True -o Acquire::https::Timeout=30 -o Acquire::ftp::Timeout=30
+apt-get -q install -y docker-ce=$docker_ce_version
+apt-get -q install -y kubelet=$kubernetes_tools_version kubeadm=$kubernetes_tools_version kubectl=$kubernetes_tools_version kubernetes-cni=$kubernetes_cni_version
+systemctl restart docker
+while [ `systemctl is-active docker` != 'active' ]; do echo 'waiting for docker'; sleep 5; done
+
+echo 'installing required software for NFS'
+apt-get -q install -y nfs-common nfs-kernel-server
+systemctl stop nfs-kernel-server.service
+systemctl disable nfs-kernel-server.service
+
+# prevent updates to software that CSE depends on
+apt-mark hold open-vm-tools
+apt-mark hold docker-ce
+apt-mark hold kubelet
+apt-mark hold kubeadm
+apt-mark hold kubectl
+apt-mark hold kubernetes-cni
+apt-mark hold nfs-common
+apt-mark hold nfs-kernel-server
+apt-mark hold shim-signed
+
+echo 'upgrading the system'
+apt-get -q update -o Acquire::Retries=3 -o Acquire::http::No-Cache=True -o Acquire::http::Timeout=30 -o Acquire::https::No-Cache=True -o Acquire::https::Timeout=30 -o Acquire::ftp::Timeout=30
+apt-get -y -q -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
+
+# Download weave.yml to /root/weave_v2-8-1.yml
+export kubever=$(kubectl version --client | base64 | tr -d '\n')
+wget --no-verbose -O $versioned_weave_file "https://cloud.weave.works/k8s/net?k8s-version=$kubever&v=$weave_version"
+
+# /etc/machine-id must be empty so that new machine-id gets assigned on boot (in our case boot is vApp deployment)
+# https://jaylacroix.com/fixing-ubuntu-18-04-virtual-machines-that-fight-over-the-same-ip-address/
+truncate -s 0 /etc/machine-id
+rm /var/lib/dbus/machine-id || :
+ln -fs /etc/machine-id /var/lib/dbus/machine-id || : # dbus/machine-id is symlink pointing to /etc/machine-id
+
+sync
+sync
+echo 'customization completed'
