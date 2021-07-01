f970b7c9de4cb2c0c040c1d26b560b4f81267726
Andrew Ni <niandrew7@gmail.com>
2021-06-30 19:21:28 -0700

    whitespace
    Signed-off-by: Andrew Ni <niandrew7@gmail.com>

--- scripts/ubuntu-16.04_k8-1.20_weave-2.6.5_rev1/mstr.sh
+++ scripts/ubuntu-16.04_k8-1.20_weave-2.6.5_rev1/mstr.sh
@@ -3,6 +3,7 @@ set -e
 while [ `systemctl is-active docker` != 'active' ]; do echo 'waiting for docker'; sleep 5; done
 
 kubeadm init --kubernetes-version=v1.20.6 > /root/kubeadm-init.out
+
 mkdir -p /root/.kube
 cp -f /etc/kubernetes/admin.conf /root/.kube/config
 chown $(id -u):$(id -g) /root/.kube/config
