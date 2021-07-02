76391b238fef3c9b722f71442a9f0f05b294def1
rocknes <aritra.iitkgp@gmail.com>
2021-07-02 12:33:10 -0700

    Add v2.0.0 template definition and scripts. (#45)
    Bring v2.0.0 style template declaration and scripts folder from 'upgrades' branch to 'master' branch.

--- scripts_v2/ubuntu-16.04_k8-1.21_weave-2.8.1_rev1/cust.sh
+++ scripts_v2/ubuntu-16.04_k8-1.21_weave-2.8.1_rev1/cust.sh
@@ -22,7 +22,14 @@ systemctl restart networking.service
 while [ `systemctl is-active networking` != 'active' ]; do echo 'waiting for network'; sleep 5; done
 
 echo 'installing kubernetes'
+
+docker_ce_version=5:20.10.7~3-0~ubuntu-xenial
+kubernetes_tools_version=1.21.2-00
+kubernetes_cni_version=0.8.7-00
+weave_version=2.8.1
+versioned_weave_file="/root/weave_v$(echo $weave_version | sed -r 's/\./\-/g').yml"
 export DEBIAN_FRONTEND=noninteractive
+
 apt-get -q update -o Acquire::Retries=3 -o Acquire::http::No-Cache=True -o Acquire::http::Timeout=30 -o Acquire::https::No-Cache=True -o Acquire::https::Timeout=30 -o Acquire::ftp::Timeout=30
 apt-get -q install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -32,9 +39,8 @@ deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get -q update -o Acquire::Retries=3 -o Acquire::http::No-Cache=True -o Acquire::http::Timeout=30 -o Acquire::https::No-Cache=True -o Acquire::https::Timeout=30 -o Acquire::ftp::Timeout=30
-apt-get -q install -y docker-ce=5:19.03.15~3-0~ubuntu-xenial
-apt-get -q install -y kubelet=1.20.6-00 kubeadm=1.20.6-00 kubectl=1.20.6-00 kubernetes-cni=0.8.7-00
-
+apt-get -q install -y docker-ce=$docker_ce_version
+apt-get -q install -y kubelet=$kubernetes_tools_version kubeadm=$kubernetes_tools_version kubectl=$kubernetes_tools_version kubernetes-cni=$kubernetes_cni_version
 systemctl restart docker
 while [ `systemctl is-active docker` != 'active' ]; do echo 'waiting for docker'; sleep 5; done
 
@@ -52,14 +58,15 @@ apt-mark hold kubectl
 apt-mark hold kubernetes-cni
 apt-mark hold nfs-common
 apt-mark hold nfs-kernel-server
+apt-mark hold shim-signed
 
 echo 'upgrading the system'
 apt-get -q update -o Acquire::Retries=3 -o Acquire::http::No-Cache=True -o Acquire::http::Timeout=30 -o Acquire::https::No-Cache=True -o Acquire::https::Timeout=30 -o Acquire::ftp::Timeout=30
 apt-get -y -q -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
 
-# Download weave.yml to /root/weave_v2-6-5.yml
+# Download weave.yml to /root/weave_v2-8-1.yml
 export kubever=$(kubectl version --client | base64 | tr -d '\n')
-wget --no-verbose -O /root/weave_v2-6-5.yml "https://cloud.weave.works/k8s/net?k8s-version=$kubever&v=2.6.5"
+wget --no-verbose -O $versioned_weave_file "https://cloud.weave.works/k8s/net?k8s-version=$kubever&v=$weave_version"
 
 # /etc/machine-id must be empty so that new machine-id gets assigned on boot (in our case boot is vApp deployment)
 # https://jaylacroix.com/fixing-ubuntu-18-04-virtual-machines-that-fight-over-the-same-ip-address/
