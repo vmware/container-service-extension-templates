74b4cba6c85af858840c409b1e28a092a6e033c6
Andrew Ni <niandrew7@gmail.com>
2022-02-01 17:28:26 -0800

    Update
    Signed-off-by: Andrew Ni <niandrew7@gmail.com>

--- scripts_v2/ubuntu-20.04_k8-1.23_weave-2.8.1_rev1/cust.sh
+++ scripts_v2/ubuntu-20.04_k8-1.23_weave-2.8.1_rev1/cust.sh
@@ -31,6 +31,7 @@ while [ `systemctl is-active systemd-networkd` != 'active' ]; do echo 'waiting f
 echo 'installing kubernetes'
 
 docker_ce_version=5:20.10.12~3-0~ubuntu-focal
+containerd_version=1.4.12-1
 kubernetes_tools_version=1.23.3-00
 kubernetes_cni_version=0.8.7-00
 weave_version=2.8.1
@@ -46,19 +47,52 @@ deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get -q update -o Acquire::Retries=3 -o Acquire::http::No-Cache=True -o Acquire::http::Timeout=30 -o Acquire::https::No-Cache=True -o Acquire::https::Timeout=30 -o Acquire::ftp::Timeout=30
-apt-get -q install -y docker-ce=$docker_ce_version
+apt-get -q install -y docker-ce=$docker_ce_version docker-ce-cli=$docker_ce_version containerd.io=$containerd_version
 apt-get -q install -y kubelet=$kubernetes_tools_version kubeadm=$kubernetes_tools_version kubectl=$kubernetes_tools_version kubernetes-cni=$kubernetes_cni_version
 
 # https://github.com/kubernetes/kubeadm/issues/1893
-cat <<EOF > /etc/docker/daemon.json
-{
-    "exec-opts": ["native.cgroupdriver=systemd"]
-}
-EOF
+# this is required if using dockershim as the cri-socket; we're switching to use containerd
+# cat <<EOF > /etc/docker/daemon.json
+# {
+#     "exec-opts": ["native.cgroupdriver=systemd"]
+# }
+# EOF
 systemctl enable docker
 systemctl daemon-reload
 systemctl restart docker
 while [ `systemctl is-active docker` != 'active' ]; do echo 'waiting for docker'; sleep 5; done
+
+# use containerd as CRI runtime
+# https://kubernetes.io/docs/setup/production-environment/container-runtimes/
+cat <<EOF | tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+
+modprobe overlay
+modprobe br_netfilter
+
+# Setup required sysctl params, these persist across reboots.
+cat <<EOF | tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+# Apply sysctl params without reboot
+sysctl --system
+mkdir -p /etc/containerd
+containerd config default | tee /etc/containerd/config.toml
+
+# Use systemd cgroup driver
+# https://kubernetes.io/docs/setup/production-environment/container-runtimes/
+sed '/containerd.runtimes.runc.options/a \            SystemdCgroup = true' /etc/containerd/config.toml > /etc/containerd/config.toml
+systemctl restart containerd
+while [ `systemctl is-active containerd` != 'active' ]; do echo 'waiting for containerd'; sleep 5; done
+
+# Kubeadm uses default CRI, which is dockershim currently
+# Change default CRI endpoint to containerd (recommended by crictl)
+# crictl config --set runtime-endpoint=/run/containerd/containerd.sock
 
 echo 'installing required software for NFS'
 apt-get -q install -y nfs-common nfs-kernel-server
@@ -67,7 +101,10 @@ systemctl disable nfs-kernel-server.service
 
 # prevent updates to software that CSE depends on
 apt-mark hold open-vm-tools
+apt-mark hold docker
 apt-mark hold docker-ce
+apt-mark hold docker-ce-cli
+apt-mark hold containerd.io
 apt-mark hold kubelet
 apt-mark hold kubeadm
 apt-mark hold kubectl
