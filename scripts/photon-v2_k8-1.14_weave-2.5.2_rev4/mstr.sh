dda9d452282d23ec48683656c9bfcf7a012d753c
Sakthi Sundaram <sakthisunda@yahoo.com>
2022-04-04 09:19:25 -0700

    [VCDA-3446] Fix the cni image location (#52)
    * Fix the cni image location
    
    Signed-off-by: Sakthi Sundaram <sakthisunda@yahoo.com>
    
    * template.yaml changes for weave fix
    
    Signed-off-by: Sakthi Sundaram <sakthisunda@yahoo.com>

---
+++ scripts/photon-v2_k8-1.14_weave-2.5.2_rev4/mstr.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+while [ `systemctl is-active docker` != 'active' ]; do echo 'waiting for docker'; sleep 5; done
+kubeadm init --kubernetes-version=v1.14.10 > /root/kubeadm-init.out
+mkdir -p /root/.kube
+cp -f /etc/kubernetes/admin.conf /root/.kube/config
+chown $(id -u):$(id -g) /root/.kube/config
+
+export kubever=$(kubectl version --client | base64 | tr -d '\n')
+wget --no-verbose -O /root/weave.yml "https://cloud.weave.works/k8s/net?k8s-version=$kubever&v=2.5.2"
+# Fix the weave image location
+sed -i 's/ghcr.io\/weaveworks\/launcher/docker.io\/weaveworks/g' /root/weave.yml
+kubectl apply -f /root/weave.yml
+systemctl restart kubelet
+while [ `systemctl is-active kubelet` != 'active' ]; do echo 'waiting for kubelet'; sleep 5; done
---
+++ scripts/ubuntu-16.04_k8-1.20_weave-2.6.5_rev3/mstr.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+while [ `systemctl is-active docker` != 'active' ]; do echo 'waiting for docker'; sleep 5; done
+kubeadm init --kubernetes-version=v1.20.6 > /root/kubeadm-init.out
+mkdir -p /root/.kube
+cp -f /etc/kubernetes/admin.conf /root/.kube/config
+chown $(id -u):$(id -g) /root/.kube/config
+
+export kubever=$(kubectl version --client | base64 | tr -d '\n')
+kubectl apply -f /root/weave_v2-6-5.yml
+systemctl restart kubelet
+while [ `systemctl is-active kubelet` != 'active' ]; do echo 'waiting for kubelet'; sleep 5; done
